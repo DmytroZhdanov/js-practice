7424b51bca1afb39bb26fe1a4dbaefa23727f769
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-04-19 15:21:13 +0200

    Solves task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const ingredientsList = document.querySelector("#ingredients");
+const ingredientsArray = ingredients.map(ingredient => {
+  const ingredientsItem = document.createElement("li");
+
+  ingredientsItem.textContent = ingredient;
+  return ingredientsItem;
+})
+
+ingredientsList.append(...ingredientsArray);
