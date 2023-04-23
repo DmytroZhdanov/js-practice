4e4e1dfff65e854d30f79f2771a2169e9a41901b
DmytroZhdanov <119117057+DmytroZhdanov@users.noreply.github.com>
2023-04-23 14:10:53 +0200

    Ammends solution for task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,8 @@ const control = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
 control.addEventListener("input", onControlChange);
+text.style.fontSize = `${control.value}px`;
 
 function onControlChange(event) {
-  text.style.fontSize = `${event.currentTarget.value}px`
+  text.style.fontSize = `${event.currentTarget.value}px`;
 }
